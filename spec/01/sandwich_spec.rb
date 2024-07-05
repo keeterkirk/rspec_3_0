de46bfa26071a7b6cd362c99a90ba3cba920f228
Kirk Keeter <kirkkeeter@gmail.com>
2024-07-05 18:19:09 -0500

    More of Chapter 2

--- spec/01/sandwich_spec.rb
+++ spec/01/sandwich_spec.rb
@@ -42,3 +42,21 @@ RSpec.describe 'Another Ideal Sandwich' do
   end
 end
 
+RSpec.describe 'An ideal sandwich' do
+
+  let(:sandwich) { Sandwich.new('delicious', []) }
+
+  it 'is delicious' do
+    taste = sandwich.taste
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+
+  end
+end
+
