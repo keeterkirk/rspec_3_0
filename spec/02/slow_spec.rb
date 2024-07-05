f0abc47884a50625dff931a37f5c477d4b6a3146
Kirk Keeter <kirkkeeter@gmail.com>
2024-07-05 18:53:05 -0500

    Ch 02 - Slow specs example

---
+++ spec/02/slow_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe 'The sleep() method' do
+  it('can sleep for 0.1 second') { sleep 0.1 }
+  it('can sleep for 0.2 second') { sleep 0.2 }
+  it('can sleep for 0.3 second') { sleep 0.3 }
+  it('can sleep for 0.4 second') { sleep 0.4 }
+  it('can sleep for 0.5 second') { sleep 0.5 }
+  it('can sleep for 0.6 second') { sleep 0.6 }
+  it('can sleep for 0.7 second') { sleep 0.7 }
+  it('can sleep for 0.8 second') { sleep 0.8 }
+  it('can sleep for 0.9 second') { sleep 0.9 }
+  it('can sleep for 1.0 second') { sleep 1.0 }
+end
