7ddd0b5345bf85e070ecbac2d6ec2deedaf08c0b
Kirk Keeter <kirkkeeter@gmail.com>
2024-07-05 15:46:36 -0500

    Hooks example with Instance Variables in setup

--- spec/01/sandwich_spec.rb
+++ spec/01/sandwich_spec.rb
@@ -26,3 +26,19 @@ RSpec.describe 'An ideal sandwich' do
     expect(toppings).not_to be_empty
   end
 end
+
+RSpec.describe 'Another Ideal Sandwich' do
+  before { @sandwich= Sandwich.new('delicious', []) }
+
+  it 'is delicious' do
+    taste = @sandwich.taste
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
+    expect(toppings).not_to be_empty
+  end
+end
+
