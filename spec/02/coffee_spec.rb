228b7acb90b1c256492ff04ecea229a1f9d836fe
Kirk Keeter <kirkkeeter@gmail.com>
2024-07-05 18:46:55 -0500

    More 2

---
+++ spec/02/coffee_spec.rb
@@ -0,0 +1,37 @@
+class Coffee
+
+  attr_accessor :price
+  def ingredients
+    @ingredients ||= []
+  end
+
+  def add(ingredient)
+    ingredients << ingredient
+    self.price += 0.25
+  end
+
+  def price
+    @price ||= 1.00
+  end
+end
+
+
+RSpec.describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price).to eq(1.00)
+  end
+
+  context 'with milk' do
+    before { coffee.add :milk }
+
+    it 'has milk' do
+      expect(coffee.ingredients).to include(:milk)
+    end
+
+    it 'costs $1.25' do
+      expect(coffee.price).to eq(1.25)
+    end
+  end
+end
